551d65135ae9d3eceaee45c57bcf7f4c083a1022
javaClassesMayBatch <sgr.p.h.w@gmail.com>
2018-05-13 14:48:39 +0530

    comment example

---
+++ try/EvenOdd.java
@@ -0,0 +1,17 @@
+package may6; //package is a keyword may6 is an identifier
+
+public class EvenOdd { //
+	public static void main(String[] s) {
+		int i;
+		i = 6;
+		int factor;
+		factor = 5;
+		if(i%factor != 0) {
+			System.out.println(i+" is not divisible by factor "+factor);
+			System.out.println(12345678);
+		}else {
+			System.out.println(i+" is divisible\t by factor "+factor);
+		}
+	}
+
+}
